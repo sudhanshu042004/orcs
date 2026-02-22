a44783d217a5d30ce6988af3a97bd291f4307513
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2026-02-23 01:18:26 +0530

    updated the user type

--- backend/types/types.go
+++ backend/types/types.go
@@ -1,6 +1,10 @@
 package types
 
 type User struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
+	Id       int64  `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Avatar   string `json:"avatar"`
+	RepoUrl  string `json:"repo_url"`
 }
