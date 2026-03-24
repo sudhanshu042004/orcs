04a7a4ee47a3aa49d7baf4e466c8aae6068d84e0
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2026-03-24 20:38:44 +0530

    add the function to find user in user repository

--- backend/internal/repository/user.go
+++ backend/internal/repository/user.go
@@ -10,7 +10,6 @@ import (
 func CreateUser(username string, name string, email string, avatar string, repoUrl string) (types.JwtPayload, error) {
 	var userId int64
 	q := `INSERT INTO users(username,name,email,avatar,repo_url) VALUES($1,$2,$3,$4,$5) RETURNING id`
-	fmt.Println(repoUrl)
 	err := database.DB.QueryRow(q, username, name, email, avatar, repoUrl).Scan(&userId)
 	if err != nil {
 		fmt.Printf("error while creating user %s\n", err.Error())
@@ -18,4 +17,16 @@ func CreateUser(username string, name string, email string, avatar string, repoU
 	}
 
 	return types.JwtPayload{Id: userId, Email: email}, nil
+}
+
+func FindUser(email string) (types.User, error) {
+	var existingUser types.User
+	q := `SELECT username,name,avatar,id,repo_url FROM users WHERE email = $1`
+
+	err := database.DB.QueryRow(q, email).Scan(&existingUser.Username, &existingUser.Name, &existingUser.Avatar, &existingUser.Id, &existingUser.RepoUrl)
+	if err != nil {
+		return types.User{}, err
+	}
+
+	return existingUser, nil
 }
