66497594fd29d288a82e98d70ce5e5036e8bfe44
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2026-04-11 01:14:21 +0530

    added auth context

---
+++ website/src/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+interface AuthContext {
+    user : User | null;
+    setUser : (user:User | null)=>void
+}
+
+export const AuthContext = createContext<AuthContext>({
+    user:null,
+    setUser: ()=>null,
+})
