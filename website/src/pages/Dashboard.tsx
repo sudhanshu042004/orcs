ab58f9fa657cfa50613f2042a804feeaf7cf8b48
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2026-04-14 00:36:44 +0530

    added the user specific avatar

--- website/src/pages/Dashboard.tsx
+++ website/src/pages/Dashboard.tsx
@@ -1,8 +1,14 @@
+import { UserAvatar } from "@/components/UserAvatar";
+import { useAuth } from "../hooks/useAuth";
 
 const Dashboard = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+     <UserAvatar/>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
