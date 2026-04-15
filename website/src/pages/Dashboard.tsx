99c3e0b0efef0b2e685e1fe267f2360cd3b70b64
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2026-04-15 23:42:58 +0530

    added the font

--- website/src/pages/Dashboard.tsx
+++ website/src/pages/Dashboard.tsx
@@ -5,8 +5,19 @@ const Dashboard = () => {
   const { user } = useAuth();
   console.log(user);
   return (
-    <div>
-     <UserAvatar/>
+    <div className="mx-auto max-w-7xl pt-10" >
+      <div className="flex justify-between row">
+        <div className="text-2xl">{user?.name}</div>
+        <div>
+          <UserAvatar />
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <button>deploy you project</button>
+        </div>
+      </div>
     </div>
   );
 };
